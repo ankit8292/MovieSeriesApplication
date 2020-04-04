77303398002ca48d9ad3d5c5f020404949c6561c
Ankit <ankit@ankit.com>
2020-04-04 16:26:27 +0530

    Updates

---
+++ precache-manifest.d8523b2345b522a3c811d649ebfe5aff.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9042887e35ca8bfc7e13dfebf7799246",
+    "url": "/MovieSeriesApplication/index.html"
+  },
+  {
+    "revision": "18741e01276ea868e320",
+    "url": "/MovieSeriesApplication/static/css/main.be015b1a.chunk.css"
+  },
+  {
+    "revision": "a5f7af2fe8ac430ee602",
+    "url": "/MovieSeriesApplication/static/js/2.0c98eceb.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/MovieSeriesApplication/static/js/2.0c98eceb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "18741e01276ea868e320",
+    "url": "/MovieSeriesApplication/static/js/main.ad9358c4.chunk.js"
+  },
+  {
+    "revision": "114fc13934c419d05e5b",
+    "url": "/MovieSeriesApplication/static/js/runtime-main.1216dd3b.js"
+  }
+]);
